f175c8ebd827b0ba9f8895f6d7aad0898375a5c6
elendur19 <mislavrobic@gmail.com>
2021-01-04 21:53:31 +0100

    added custom temperature unit

--- microservices/aggregator-microservice/src/main/java/hr/rasus/microservice/aggregator/service/AggregatorService.java
+++ microservices/aggregator-microservice/src/main/java/hr/rasus/microservice/aggregator/service/AggregatorService.java
@@ -6,6 +6,7 @@ import hr.rasus.microservice.aggregator.model.HumidityRepresentation;
 import hr.rasus.microservice.aggregator.model.TemperatureRepresentation;
 import lombok.AllArgsConstructor;
 
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -18,21 +19,39 @@ public class AggregatorService {
         this.restTemplateImplementation = restTemplateImplementation;
         temperatureUrl = null;
         humidityUrl = null;
+        temperatureUnit = null;
     }
     @Value("${temperature-url}")
     private final String temperatureUrl;
     @Value("${humidity-url}")
     private final String humidityUrl;
+    @Value("${temperature-unit}")
+    private final String temperatureUnit;
 
     public AggregatedRepresentation getAggregatedMeasurements() {
         AggregatedRepresentation aggregatedRepresentation = new AggregatedRepresentation();
         TemperatureRepresentation temperatureRepresentation = restTemplateImplementation.getTemperatureRepresentation(temperatureUrl);
 
         HumidityRepresentation humidityRepresentation = restTemplateImplementation.getHumidityRepresentation(humidityUrl);
+        // set temperature
+        setAggregatedTemperature(aggregatedRepresentation, temperatureRepresentation.getTemperature());
 
-        aggregatedRepresentation.setTemperature(temperatureRepresentation.getTemperature());
         aggregatedRepresentation.setHumidity(humidityRepresentation.getHumidity());
 
         return aggregatedRepresentation;
+    }
+
+    @SneakyThrows
+    private void setAggregatedTemperature(AggregatedRepresentation aggregatedRepresentation, double temperature) {
+
+        if (temperatureUnit.equals("Celsius")) {
+            // return temperature in Celsius
+            aggregatedRepresentation.setTemperature(temperature);
+        } else if (temperatureUnit.equals("Kelvin")) {
+            aggregatedRepresentation.setTemperature(temperature + 273.15);
+        } else {
+            // wrong temperature format
+        throw new Exception("Temperature unit must have valid format!");
+        }
     }
 }
