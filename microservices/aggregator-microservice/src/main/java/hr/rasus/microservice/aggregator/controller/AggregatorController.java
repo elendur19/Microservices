089c51a60363ae2824f67242f28ff35644aed781
elendur19 <mislavrobic@gmail.com>
2021-01-05 22:46:39 +0100

    added dependecy for config cloud

--- microservices/aggregator-microservice/src/main/java/hr/rasus/microservice/aggregator/controller/AggregatorController.java
+++ microservices/aggregator-microservice/src/main/java/hr/rasus/microservice/aggregator/controller/AggregatorController.java
@@ -3,17 +3,28 @@ package hr.rasus.microservice.aggregator.controller;
 import hr.rasus.microservice.aggregator.model.AggregatedRepresentation;
 import hr.rasus.microservice.aggregator.service.AggregatorService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor
+@RefreshScope
 public class AggregatorController {
 
     private final AggregatorService aggregatorService;
 
+    @Value("${pozdrav}")
+    private String tempUnit;
+
+    public AggregatorController(AggregatorService aggregatorService) {
+        this.aggregatorService = aggregatorService;
+        tempUnit = null;
+    }
+
     @GetMapping("/readings")
     public AggregatedRepresentation getAggregatedMeasurements() {
+        String a = tempUnit;
         return aggregatorService.getAggregatedMeasurements();
     }
 
