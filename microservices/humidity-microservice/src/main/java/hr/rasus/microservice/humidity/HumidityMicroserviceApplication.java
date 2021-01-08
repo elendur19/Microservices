d73176bffe54e551b3913280ef42417bf8783f11
ivanivankovic <ivankovicivan11@gmail.com>
2021-01-08 01:34:12 +0100

    Eureka server setup

--- microservices/humidity-microservice/src/main/java/hr/rasus/microservice/humidity/HumidityMicroserviceApplication.java
+++ microservices/humidity-microservice/src/main/java/hr/rasus/microservice/humidity/HumidityMicroserviceApplication.java
@@ -1,9 +1,17 @@
 package hr.rasus.microservice.humidity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HumidityMicroserviceApplication {
 
     public static void main(String[] args) {
@@ -11,4 +19,17 @@ public class HumidityMicroserviceApplication {
         SpringApplication.run(HumidityMicroserviceApplication.class, args);
     }
 
+}
+
+@RestController
+class EurekaClientRestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/clients/{applicationName}")
+    public @ResponseBody
+    String getClientsByApplicationName(@PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName).get(0).getUri().toString();
+    }
 }
--- microservices/temperature-microservice/src/main/java/hr/rasus/microservice/temperature/TemperatureMicroserviceApplication.java
+++ microservices/temperature-microservice/src/main/java/hr/rasus/microservice/temperature/TemperatureMicroserviceApplication.java
@@ -1,9 +1,19 @@
 package hr.rasus.microservice.temperature;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TemperatureMicroserviceApplication {
 
     public static void main(String[] args) {
@@ -11,4 +21,17 @@ public class TemperatureMicroserviceApplication {
         SpringApplication.run(TemperatureMicroserviceApplication.class, args);
     }
 
+}
+
+@RestController
+class EurekaClientRestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/clients/{applicationName}")
+    public @ResponseBody
+    String getClientsByApplicationName(@PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName).get(0).getUri().toString();
+    }
 }
